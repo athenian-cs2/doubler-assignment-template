845cc1a901566630f3bc796a6501bef1bdcef3c0
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:07:19 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,9 +1,9 @@
 public class MyMain {
 
-    public static void main(String[] args) {
-        Doubler doubler = new Doubler();
+  public static void main(String[] args) {
+    Doubler doubler = new Doubler();
 
-        System.out.println(doubler.doubleIt(4));
-        System.out.println(doubler.doubleIt(7));
-    }
+    System.out.println(doubler.doubleIt(4));
+    System.out.println(doubler.doubleIt(7));
+  }
 }
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,18 +1,17 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class MyTests {
 
-    @Test
-    public void testDoubler() {
-        Doubler doubler = new Doubler();
+  @Test
+  public void testDoubler() {
+    Doubler doubler = new Doubler();
 
-        // assert statements
-        assertEquals(16, doubler.doubleIt(8));
-        assertEquals(24, doubler.doubleIt(12));
-        assertEquals(0, doubler.doubleIt(0));
-        assertEquals(-2, doubler.doubleIt(-1));
-    }
-
+    // assert statements
+    assertEquals(16, doubler.doubleIt(8));
+    assertEquals(24, doubler.doubleIt(12));
+    assertEquals(0, doubler.doubleIt(0));
+    assertEquals(-2, doubler.doubleIt(-1));
+  }
 }
